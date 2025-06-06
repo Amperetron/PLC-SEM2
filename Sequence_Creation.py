b1bfee2b0010672b9dc1a924cef4e92bc99b7ce5
Simplify_ <amitharavindpai@gmail.com>
2025-06-07 00:08:04 +0530

    Changes to sequence creation

--- Sequence_Creation.py
+++ Sequence_Creation.py
@@ -1,20 +1,26 @@
 import random 
 
 def sequence_creation(n):
-  seq = ""
+  seq = []
   for i in range (n):
+    print(f"{i}.",end="")
+    char = ""
+
     l_choice = random.randint(0,1)  #To choose one between uppercase and lowercase for each charcter
 
     ch_choice = random.randint(65,90) #To choose an ascii value of uppercase letter for each letter in the sequence 
 
     match l_choice:
       case 0: #For upppercase
-        seq += chr(ch_choice) #We  simply concatenate to seq by converting to ascii
-        seq += " "
+        char = chr(ch_choice)
+        print(char)
+        seq.append(char) #We  simply concatenate to seq by converting to ascii
 
       case 1: #For lowercase
-        seq += chr(ch_choice + 32) #if choice is lowercase, we first convert uppercase ascii to lowercase and concatenate
-        seq += " "
+        char = chr(ch_choice + 32)
+        print(char)
+        seq.append(char) #if choice is lowercase, we first convert uppercase ascii to lowercase and concatenate
 
-  return seq
+  print(seq)
+
  
